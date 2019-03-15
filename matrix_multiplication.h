c5e44e9ae2f8551088840f8e28a8e8afd2369550
jysh1214 <jyxemperor@gmail.com>
2019-03-16 03:03:46 +0800

    Create matrix_multiplication.h

---
+++ matrix_multiplication.h
@@ -0,0 +1,28 @@
+#ifndef MATRIX_MULTIPLICATION_H
+#define MATRIX_MULTIPLICATION_H
+
+#include <string>
+
+template <typename T, size_t ar, size_t al, size_t br,size_t bl,size_t cr,size_t cl>
+void matrixMultiplication(T (& a)[ar][al], T (& b)[br][bl], T (& c)[cr][cl]) // NXM * MXR
+{
+    if (al != br || ar != cr || al != cl)
+    {
+        throw std::string("Error.");
+    }
+
+    int N = ar; int M = br; int R = cl;
+
+    for (int i=0; i<N ; i++)
+    {
+        for (int j=0; j<R; j++)
+        {
+            c[i][j] = 0;
+            for (int k=0; k<M; k++)
+            {
+                c[i][j] += a[i][k] * b[k][j]; 
+            } 
+        }
+    }
+}
+#endif
